a38f9d7000c460e0474272539abe0e6a1d40dbbc
ralphcomplido <rcomplido.la.usa@gmail.com>
2018-01-18 23:12:51 -0800

    Create a React app and publish it to GitHub Pages

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <header className="header">
+    <h1>Don't Forget Me Game!</h1>
+    <h2>
+      If you click me again, You Lose!
+    </h2>
+  </header>
+);
+
+export default Header;
